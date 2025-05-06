0ba11e126be80de68db249b30876f84e90e88244
Adrian Ściepura <adriansciepura@outlook.com>
2025-05-07 01:48:38 +0200

    DELETE THIS COMMIT TOO

--- src/math/fft.c
+++ src/math/fft.c
@@ -84,6 +84,114 @@ void fft_iterative(complex_t* data, int n, bool invert)
     }
 }
 
+// Performs multiplication of two complex numbers
+// ----- Data -----
+// num1 = a + bi
+// num2 = c + di
+// num3 = e + fi
+// num4 = g + hi
+// ----- Result -----
+// res = [num1 * num2, num3 * num4]
+static inline float32x4_t multiply_two_complex(float32x4_t abef, float32x4_t ccgg, float32x4_t ddhh)
+{
+    float32x4_t temp1, temp2, temp3;
+
+    // ----- Stage 1 -----
+
+    // temp1 = [ac, bc, eg, fg]
+    temp1 = vmulq_f32(abef, ccgg);
+    
+    // temp2 = [b, a, f, e]
+    temp2 = vrev64q_f32(abef);
+    
+    // temp2 = [bd, ad, fh, eh]
+    temp2 = vmulq_f32(temp2, ddhh);
+
+    // ----- Stage 2 -----
+    
+    // temp3 = [ac, bc, eg, fg]
+    temp3 = temp1;
+    
+    // temp1 = [ac - bd, bc - ad, eg - fh, fg - eh]
+    temp1 = vsubq_f32(temp1, temp2);
+    
+    // temp2 = [ac + bd, bc + ad, eg + fh, fg + eh]
+    temp2 = vaddq_f32(temp3, temp2);
+
+    // ----- Stage 3 -----
+
+    // result = [ac - bd, bc + ad, eg - fh, fg + eh]
+    float32x4_t result = { 
+        vgetq_lane_f32(temp1, 0),
+        vgetq_lane_f32(temp2, 1),
+        vgetq_lane_f32(temp1, 2),
+        vgetq_lane_f32(temp2, 3)
+    };
+
+    return result;
+}
+
+
+void fft_iterative_neon_fixed(complex_t* data, int n, bool invert)
+{
+    for(int i = 1, j = 0; i < n; i++)
+    {
+        int bit = n >> 1;
+        for(; j & bit; bit >>= 1)
+            j ^= bit;
+        
+        j ^= bit;
+        if(i < j)
+        {
+            complex_t tmp = data[i];
+            data[i] = data[j];
+            data[j] = tmp;
+        }
+    }
+
+    float buffer[4] __attribute__((aligned(16)));
+    
+    uint64_t* buffer_qword_ptr = (uint64_t*)&buffer;
+    uint64_t* data_qword_ptr = (uint64_t*)data;
+
+    for(int len = 2; len <= n; len <<= 1)
+    {
+        float angle = 2 * PI / len * (invert ? -1 : 1);
+        float angle_sin = sinf(angle);
+        float angle_cos = cosf(angle);
+
+        float32x4_t wlen = { angle_cos, angle_sin, angle_cos, angle_sin };
+
+        for(int i = 0; i < n; i += len)
+        {    
+            float32x4_t w_re = { 1, 1, angle_cos, angle_cos };
+            float32x4_t w_im = { 0, 0, angle_sin, angle_sin };
+
+            for(int j = 0; j < len / 2; j += 2)
+            {
+                buffer_qword_ptr[0] = data_qword_ptr[i + j];
+                buffer_qword_ptr[1] = data_qword_ptr[i + j + 1];    
+                float32x4_t u = vld1q_f32(buffer);
+
+                buffer_qword_ptr[0] = data_qword_ptr[i + j + (len / 2)];
+                buffer_qword_ptr[1] = data_qword_ptr[i + j + (len / 2) + 1];    
+                float32x4_t v = vld1q_f32(buffer);
+
+                v = multiply_two_complex(v, w_re, w_im);
+                
+                vst1q_f32((float*)&data[i + j], vaddq_f32(u, v));
+                vst1q_f32((float*)&data[i + j + (len / 2)], vsubq_f32(u, v));
+
+                vst1q_f32(buffer, multiply_two_complex(wlen, w_re, w_im));
+                w_re = (float32x4_t) { buffer[0], buffer[0], buffer[2], buffer[2] };
+                w_im = (float32x4_t) { buffer[1], buffer[1], buffer[3], buffer[3] };
+            }
+        }
+    }
+}
+
+
+
 void fft_iterative_neon(complex_t* data, int n, bool invert)
 {
     for(int i = 1, j = 0; i < n; i++)
@@ -101,13 +209,15 @@ void fft_iterative_neon(complex_t* data, int n, bool invert)
         }
     }
 
-    float *data_real;
-    float *data_imag;
-    if (posix_memalign((void**)&data_real, 16, sizeof(float) * n) != 0 ||
+
+    float *data_real = (float*)aligned_alloc(16, sizeof(float) * (n+3));
+    float *data_imag = (float*)aligned_alloc(16, sizeof(float) * (n+3));
+
+/*    if (posix_memalign((void**)&data_real, 16, sizeof(float) * n) != 0 ||
         posix_memalign((void**)&data_imag, 16, sizeof(float) * n) != 0) {
         printf("Memory alignment failed\n");
         exit(1);
-    }
+    }*/
     for(int i = 0; i < n; i++)
     {
         data_real[i] = data[i].re;
