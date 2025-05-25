e980672905441f2248a72606c0a4bf5851f6abcc
binio <binczykartur77@gmail.com>
2025-05-25 13:30:40 +0200

    Fix multiplication

--- src/math/fft.c
+++ src/math/fft.c
@@ -111,43 +111,20 @@ void fft_iterative(complex_t* data, int n, bool invert)
 // res = [num1 * num2, num3 * num4]
 static inline float32x4_t multiply_two_complex(float32x4_t abef, float32x4_t ccgg, float32x4_t ddhh)
 {
-    static uint32x4_t mask_elements_0_and_2 = { 0xFFFFFFFF, 0x00000000, 0xFFFFFFFF, 0x00000000 };
-    static uint32x4_t mask_elements_1_and_3 = { 0x00000000, 0xFFFFFFFF, 0x00000000, 0xFFFFFFFF };
- 
-    float32x4_t temp1, temp2, temp3;
-
-    // ----- Stage 1 -----
-
-    // temp1 = [ac, bc, eg, fg]
-    temp1 = vmulq_f32(abef, ccgg);
+    // Compute the two products:
+    float32x4_t prod_a = vmulq_f32(abef, ccgg);
+    float32x4_t prod_b = vmulq_f32(vrev64q_f32(abef), ddhh);
     
-    // temp2 = [b, a, f, e]
-    temp2 = vrev64q_f32(abef);
-    
-    // temp2 = [bd, ad, fh, eh]
-    temp2 = vmulq_f32(temp2, ddhh);
+    // Compute the real and imaginary parts
+    float32x4_t re = vsubq_f32(prod_a, prod_b);
+    float32x4_t im = vaddq_f32(prod_a, prod_b);
 
-    // ----- Stage 2 -----
-    
-    // temp3 = [ac, bc, eg, fg]
-    temp3 = temp1;
-    
-    // temp1 = [ac - bd, bc - ad, eg - fh, fg - eh]
-    temp1 = vsubq_f32(temp1, temp2);
-    
-    // temp2 = [ac + bd, bc + ad, eg + fh, fg + eh]
-    temp2 = vaddq_f32(temp3, temp2);
+    // Construct the result vector with:
+    // lane0 = re[0], lane1 = im[1], lane2 = re[2], lane3 = im[3]
+    float32x2_t res_lo = { vgetq_lane_f32(re, 0), vgetq_lane_f32(im, 1) };
+    float32x2_t res_hi = { vgetq_lane_f32(re, 2), vgetq_lane_f32(im, 3) };
 
-    // ----- Stage 3 -----
-
-    // temp3 = [0, 0, 0, 0]
-    temp3 = vdupq_n_f32(0);
-
-    // result = [ac - bd, bc + ad, eg - fh, fg + eh]
-    return vaddq_f32(
-        vbslq_f32(mask_elements_0_and_2, temp1, temp3),
-        vbslq_f32(mask_elements_1_and_3, temp2, temp3)
-    );
+    return vcombine_f32(res_lo, res_hi);
 }
 
 
@@ -230,114 +207,6 @@ void fft_iterative_neon_fixed(complex_t* data, int n, bool invert)
             data[i].im /= n;
         }
     }
-}
-
-
-
-void fft_iterative_neon(complex_t* data, int n, bool invert)
-{
-    for(int i = 1, j = 0; i < n; i++)
-    {
-        int bit = n >> 1;
-        for(; j & bit; bit >>= 1)
-            j ^= bit;
-        
-        j ^= bit;
-        if(i < j)
-        {
-            complex_t tmp = data[i];
-            data[i] = data[j];
-            data[j] = tmp;
-        }
-    }
-
-
-    float *data_real = (float*)aligned_alloc(16, sizeof(float) * (n+3));
-    float *data_imag = (float*)aligned_alloc(16, sizeof(float) * (n+3));
-
-/*    if (posix_memalign((void**)&data_real, 16, sizeof(float) * n) != 0 ||
-        posix_memalign((void**)&data_imag, 16, sizeof(float) * n) != 0) {
-        printf("Memory alignment failed\n");
-        exit(1);
-    }*/
-    for(int i = 0; i < n; i++)
-    {
-        data_real[i] = data[i].re;
-        data_imag[i] = data[i].im;
-    } 
-
-    for(int len = 2; len <= n; len <<= 1)
-    {
-        float angle = 2 * PI / len * (invert ? -1 : 1);
-        float wlen_re = cos(angle);
-        float wlen_im = sin(angle);
-        for(int i = 0; i < n; i += len)
-        {
-            float32x4_t w_re = vdupq_n_f32(1.0f);
-            float32x4_t w_im = vdupq_n_f32(0.0f);
-            float32x4_t wlen_re_vec = vdupq_n_f32(wlen_re);
-            float32x4_t wlen_im_vec = vdupq_n_f32(wlen_im);
-
-            for(int j = 0; j < len / 2; j += 4)
-            {
-                float32x4_t u_re = vld1q_f32(&data_real[i + j]);
-                float32x4_t u_im = vld1q_f32(&data_imag[i + j]);
-
-                // Load v (data[i + j + len / 2]) and compute w * v
-                float32x4_t v_re = vld1q_f32(&data_real[i + j + len / 2]);
-                float32x4_t v_im = vld1q_f32(&data_imag[i + j + len / 2]);
-                float32x4_t temp_re = vsubq_f32(vmulq_f32(w_re, v_re), vmulq_f32(w_im, v_im));
-                float32x4_t temp_im = vaddq_f32(vmulq_f32(w_re, v_im), vmulq_f32(w_im, v_re));
-
-                // // Update data[i + j] = u + w * v
-                vst1q_f32(&data_real[i + j], vaddq_f32(u_re, temp_re));
-                vst1q_f32(&data_imag[i + j], vaddq_f32(u_im, temp_im));
-
-                // Update data[i + j + len / 2] = u - w * v
-                // printf("i: %d, j: %d, len: %d, index: %d, n: %d\n", i, j, len, i + j + len / 2, n);
-                // if (((uintptr_t)&data_real[i + j + len / 2] % 16) != 0) {
-                //     printf("Unaligned memory address: %p\n", &data_real[i + j + len / 2]);
-                //     exit(1);
-                // }
-                
-                // if (((uintptr_t)&data_imag[i + j + len / 2] % 16) != 0) {
-                //     printf("Unaligned memory address: %p\n", &data_imag[i + j + len / 2]);
-                //     exit(1);
-                // }
-                
-                vst1q_f32(&data_real[i + j + len / 2], vsubq_f32(u_re, temp_re));
-                //vst1q_f32(&data_imag[i + j + len / 2], vsubq_f32(u_im, temp_im));
-
-                // // Update w = w * wlen
-                float32x4_t new_w_re = vsubq_f32(vmulq_f32(w_re, wlen_re_vec), vmulq_f32(w_im, wlen_im_vec));
-                float32x4_t new_w_im = vaddq_f32(vmulq_f32(w_re, wlen_im_vec), vmulq_f32(w_im, wlen_re_vec));
-                w_re = new_w_re;
-                w_im = new_w_im;
-            }
-        }
-    }
-    
-    if(invert)
-    {
-        float32x4_t n_vec = vdupq_n_f32(n);
-        for(int i = 0; i < n; i += 4)
-        {
-            float32x4_t re = vld1q_f32(&data[i].re);
-            float32x4_t im = vld1q_f32(&data[i].im);
-            vst1q_f32(&data[i].re, vdivq_f32(re, n_vec));
-            vst1q_f32(&data[i].im, vdivq_f32(im, n_vec));
-        }
-    }
-    
-    for(int i = 0; i < n; i++)
-    {
-        data[i].re = data_real[i];
-        data[i].im = data_imag[i];
-    }
-    
-    free(data_real);
-    free(data_imag);
-    
 }
 
 void cooley_tukey_ifft(complex_t* data, int n)
